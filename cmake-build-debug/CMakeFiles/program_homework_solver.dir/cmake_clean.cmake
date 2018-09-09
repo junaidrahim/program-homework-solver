36d3562f40116f88d7e56a64de49025609636bdf
JunaidRahim <junaidrahim8d@gmail.com>
2018-09-09 16:20:23 +0530

    Init

---
+++ cmake-build-debug/CMakeFiles/program_homework_solver.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/program_homework_solver.dir/main.cpp.o"
+  "program_homework_solver.pdb"
+  "program_homework_solver"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/program_homework_solver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/program_homework_solver.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for program_homework_solver.
+# This may be replaced when dependencies are built.
