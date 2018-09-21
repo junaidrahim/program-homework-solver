9d50783834e76c21637f075c0e6b678b0f359b10
JunaidRahim <junaidrahim8d@gmail.com>
2018-09-21 17:34:18 +0530

    Done with test.cpp, will add extra tests later

--- main.cpp
+++ main.cpp
@@ -11,43 +11,6 @@ using namespace std;
 
 int main() {
 
-    vector<int> data_y = {0,5,10,15,20};
-    vector<int> data_x;
-
-    for(int i=0; i<data_y.size(); i++){
-        data_x.push_back(i+1);
-    }
-
-    BasisPolynomial bp(data_x);
-    vector<poly> basis_polynomials = bp.get_all_basis_polynomials();
-
-    LagrangePolynomial lp;
-
-    vector<LagrangePolynomial::element> result = lp.get_lagrange_polynomial(data_y,basis_polynomials);
-
-    for(int j=0; j<result.size(); j++){
-        cout << result[j].multiple << " ";
-        for(int i=0; i<data_x.size()-1; i++){
-            cout << result[j].p[i] << " ";
-        }
-        cout << result[j].denominator << endl;
-    }
-
-
-    CodeGenerator cg(result);
-
-    ofstream outfile;
-
-    CodeGenerator::code_arr all_programs = cg.generate_all_lang_code();
-
-    string file = "/home/junaidrahim/Desktop/test/test.";
-
-    for(int i=0; i<6; i++){
-        outfile.open(file+all_programs.languages[i]);
-        outfile << all_programs.code[i] << endl;
-        outfile.close();
-    }
-
 
 
     return 0;
--- src/test.cpp
+++ src/test.cpp
@@ -11,8 +11,50 @@
 #include "../include/LagrangePolynomial.h"
 #include "../include/CodeGenerator.h"
 
+void generate_programs(vector<LagrangePolynomial::element> result);
+
 int main(){
 
+    vector<int> data_y = {1,4,9,16,25,36};
+    vector<int> data_x = {1,2,3,4,5,6};
+
+    BasisPolynomial bp(data_x);
+    // What is poly ?
+    // poly => ((x-2)(x-3)/2) => struct {vector<string> numerators,int denominators}
+    vector<poly> basis_polynomials = bp.get_all_basis_polynomials();
+
+    LagrangePolynomial lp;
+
+    // What is element ?
+    // (4(x-2)(x-3))/2 => struct {int multiple, vector<string> p, int denominator}
+    vector<LagrangePolynomial::element> result = lp.get_lagrange_polynomial(data_y,basis_polynomials);
+
+    for(int j=0; j<result.size(); j++){ //prints the lp in the format 4 (x-2)(x-3) 2
+        cout << result[j].multiple << " ";
+        for(int i=0; i<data_x.size()-1; i++){
+            cout << result[j].p[i] << " ";
+        }
+        cout << result[j].denominator << endl;
+    }
+
+    // if in doubt, /**/ it out
+    generate_programs(result);
 
     return 0;
+}
+
+void generate_programs(vector<LagrangePolynomial::element> result){
+
+    CodeGenerator cg(result);
+    CodeGenerator::code_arr all_programs = cg.generate_all_lang_code();
+
+    ofstream outfile;
+    string file = "/home/junaidrahim/Desktop/test/test.";
+
+    for(int i=0; i<6; i++){
+        outfile.open(file+all_programs.languages[i]);
+        outfile << all_programs.code[i] << endl;
+        outfile.close();
+    }
+
 }
